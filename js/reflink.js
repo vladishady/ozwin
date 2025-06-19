fe9d1f3de89c4fb76ea90f4046c2d69248971e9e
vladishady <tools2@clickonomy.media>
2025-06-19 21:09:49 +0200

    reflink for buttons fixed

--- js/reflink.js
+++ js/reflink.js
@@ -1,5 +1,10 @@
-$(document).ready(function(){
-  $("a").click(function(){
-    $("#reflink*").attr("href", "https://link.totalaffiliates.com/c/509101");
+$(document).ready(function() {
+  $(document).on('click', 'a[id^="reflink1"]', function() {
+    $(this).attr("href", "https://link.totalaffiliates.com/c/504863");
+  });
+
+  $(document).on('click', '[id="reflink"]', function() {
+    const url = 'https://link.totalaffiliates.com/c/504863';
+    window.open(url, '_blank');
   });
 });
